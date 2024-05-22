402c5e0034d91326bae5ac5e4aca781aefa21bbb
AJSolis01 <AJSolis01@outlook.com>
2024-05-22 00:12:23 -0400

    Add project files.

---
+++ Austin Solis NET Core Portfolio/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Austin_Solis_NET_Core_Portfolio.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Austin Solis NET Core Portfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Austin_Solis_NET_Core_Portfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
